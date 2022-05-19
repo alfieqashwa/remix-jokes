bd1fd1b7baa939113bd4908ecc8630082820af12
alfieqashwa <alfieqashwa@gmail.com>
2022-05-19 23:29:21 +0700

    include JavaScript on our JavaScript app. 😂

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,12 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import { Links, LiveReload, Meta, Outlet, useCatch } from "@remix-run/react";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  useCatch
+} from "@remix-run/react";
 
 import globalStylesUrl from "./styles/global.css";
 import globalMediumStylesUrl from "./styles/global-medium.css";
@@ -55,6 +62,7 @@ function Document({
       </head>
       <body>
         {children}
+        <Scripts />
         <LiveReload />
       </body>
     </html>
@@ -86,6 +94,8 @@ export function CatchBOundary() {
 }
 
 export function ErrorBoundary({ error }: { error: Error; }) {
+  // enable clg will get server-side errors logged in the browser's console 🤯
+  // console.log(error);
   return (
     <Document title="Uh-oh!">
       <div className="error-container">
